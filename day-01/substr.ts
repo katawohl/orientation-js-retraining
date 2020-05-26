de991e8d2bc08e52fe26a65c7c0cfdafe13a3868
katawohl <katawohl@gmail.com>
2020-05-26 16:38:13 +0200

    added subst

--- day-01/substr.ts
+++ day-01/substr.ts
@@ -4,8 +4,6 @@ function substr(str: string, keyWord: string) {
     return str.indexOf(keyWord);
 }
 
-// should print: `17`
 console.log(substr("this is what I'm searching in", "searching"));
 
-// should print: `-1`
 console.log(substr("this is what I'm searching in", "not"));
