c4acd1002ad4968fa82663ab9ab0ffa7d89cbdc3
katawohl <katawohl@gmail.com>
2020-05-26 13:46:26 +0200

    added hello others exercise

---
+++ day-01/hello-others/hello-others.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let name1 = "Niko";
+let name2 = "Barna";
+let name3 = "Ati";
+
+console.log("Hello, " + name1 + "!");
+console.log("Hello, " + name2 + "!");
+console.log("Hello, " + name3 + "!");
