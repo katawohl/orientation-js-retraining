b210b07da9d8964f69ecbe09dc8d45995dbb1132
katawohl <katawohl@gmail.com>
2020-05-26 15:12:32 +0200

    added basic info exercise

---
+++ day-01/basic-info/basic-info.ts
@@ -0,0 +1,6 @@
+let nameOfMe: string = "Kata";
+let age: number = 33;
+let height: number = 1.7;
+let ifMarried: boolean = true;
+
+console.log("My name is " + nameOfMe + " and I'm " + age + " old. I'm " + height + " m tall and it is " + ifMarried + " that I'm married. Have a good day.");
