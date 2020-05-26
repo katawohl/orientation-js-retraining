48e041d2370b2199a29ce244ffa3f93bbcc1ee4f
katawohl <katawohl@gmail.com>
2020-05-26 18:06:58 +0200

    added compare lenght

---
+++ arrays/compare-lenghts.ts
@@ -0,0 +1,8 @@
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (secondList.length > firstList.length){
+    console.log("The second list is longer")
+} else {
+    console.log("The first list is longer (or equal to the second one)")
+}
