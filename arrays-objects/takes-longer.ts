36eea4151a929824bb719326c94bceb8fa20bda9
katawohl <katawohl@gmail.com>
2020-05-27 12:47:33 +0200

    added takes longer

---
+++ arrays-objects/takes-longer.ts
@@ -0,0 +1,11 @@
+'use strict';
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+let stringToInsert: string = " always takes longer than what ";
+
+quote = quote.substr(0,20) + stringToInsert + quote.substr(21);
+
+console.log(quote);
