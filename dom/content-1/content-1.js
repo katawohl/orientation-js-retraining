38d6cdabaecb559296f2ae77a12b1f3ec23cdfab
katawohl <katawohl@gmail.com>
2020-05-28 08:24:27 +0200

    adde content 1

---
+++ dom/content-1/content-1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let apple = document.querySelector('.apple');
+let balloon = document.querySelector('.balloon');
+let cat = document.querySelector('.cat');
+let animal = document.querySelector('.animals');
+
+apple.innerText = animal.innerText;
+balloon.innerText = animal.innerText;
+cat.innerText = animal.innerText;
+
+apple.innerHTML = animal.innerHTML;
+balloon.innerHTML = animal.innerHTML;
+cat.innerHTML = animal.innerHTML;
