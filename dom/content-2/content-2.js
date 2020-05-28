02e5bbbeb17dbe62e416959cc30335cc03af4dfc
katawohl <katawohl@gmail.com>
2020-05-28 09:20:55 +0200

    added content-2

---
+++ dom/content-2/content-2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let fruits = ['apple', 'banana', 'cat', 'dog'];
+
+let bulletPoints = document.querySelectorAll('li');
+
+bulletPoints[0].innerText = fruits[0];
+bulletPoints[1].innerText = fruits[1];
+bulletPoints[2].innerText = fruits[2];
+bulletPoints[3].innerText = fruits[3];
