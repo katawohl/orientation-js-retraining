41a0eb5eca2421ed61094afe0c8b47f8d0bfb727
katawohl <katawohl@gmail.com>
2020-05-28 08:27:04 +0200

    corrected variable name from ifMarried to isMarried

--- basics-of-js/basic-info.ts
+++ basics-of-js/basic-info.ts
@@ -1,6 +1,6 @@
 let nameOfMe: string = "Kata";
 let age: number = 33;
 let height: number = 1.7;
-let ifMarried: boolean = true;
+let isMarried: boolean = true;
 
-console.log("My name is " + nameOfMe + " and I'm " + age + " old. I'm " + height + " m tall and it is " + ifMarried + " that I'm married. Have a good day.");
+console.log("My name is " + nameOfMe + " and I'm " + age + " old. I'm " + height + " m tall and it is " + isMarried + " that I'm married. Have a good day.");
