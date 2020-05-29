e8f74b217d84eb73ba42f074b3fbc4434067523e
katawohl <katawohl@gmail.com>
2020-05-29 09:15:45 +0200

    added code to display gifs

--- ajax-fetch-api/giphy-api/giphy.js
+++ ajax-fetch-api/giphy-api/giphy.js
@@ -1,17 +1,24 @@
 'use strict';
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=VopGp0I0Fgn2Dgq029LJ0dokfh4diTp7&q=dog&limit=25&offset=0&rating=G&lang=en')
+let div = document.querySelector('#output')
+
+fetch('https://api.giphy.com/v1/gifs/search?api_key=VopGp0I0Fgn2Dgq029LJ0dokfh4diTp7&q=dog&limit=16&offset=0&rating=G&lang=en')
     .then((res) => res.json())
     .then((data) => {
         let gifs = Object.values(data)[0];
-        let list = document.querySelector('#output');
 
         gifs.forEach(gif => {
-         let listItem = document.createElement('li');
+            let gifDiv = document.createElement('div');
+            let gifImage = document.createElement('iframe');
 
-         listItem.innerText = gif.title;
+            gifImage.setAttribute('src', gif.embed_url);
+            gifImage.setAttribute('with', '480');
+            gifImage.setAttribute('height', '480');
+            gifImage.setAttribute('frameborder', '0');
 
-         list.appendChild(listItem);
+            gifDiv.appendChild(gifImage);
+
+            div.appendChild(gifDiv);
         });
 
     });
