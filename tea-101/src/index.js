6a6f4b271c878cb13b7b654ff4f95dd55aaa6cbb
katawohl <katawohl@gmail.com>
2020-05-30 11:31:38 +0200

    added landing page view

--- tea-101/src/index.js
+++ tea-101/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+const Button = () => (
+<button class="yellowButton">Check 'em out!</button>
+);
 
 const App = () => (
-    <p>HEllo world!</p>
+    <div class="mainDiv" >
+        <h1>Delicious Delights</h1>
+        <Button/>
+    </div>
 );
 
 ReactDOM.render(
