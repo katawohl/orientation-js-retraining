769fe00dd03740e04c099f98ec664f98f8a864b5
katawohl <katawohl@gmail.com>
2020-05-30 12:21:07 +0200

    added landing page button funcitonality

--- tea-101/src/index.js
+++ tea-101/src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Button = () => (
-<button class="yellowButton">Check 'em out!</button>
+const Button = (props) => (
+<button class="yellowButton" onClick={props.onClickFunction}>Check 'em out!</button>
 );
 
-const App = () => (
+class App extends React.Component {
+    state = {
+        showTeas: false,
+      };
+
+    render() {
+        const showTeasAction = () => this.setState({showTeas: true});
+        return (
+            <div class="mainDiv" >
+                <h1>Delicious Delights</h1>
+                <Button onClickFunction={showTeasAction} />
+                { this.state.showTeas && (<div>hello world!</div>) }
+            </div>
+      
+        )
+      }
+}
+
+/*const App = () => (
+    const [showTeas, setShowTeas] = useState(false);
+
     <div class="mainDiv" >
         <h1>Delicious Delights</h1>
-        <Button/>
+        <Button onClickFunction= />
     </div>
 );
+*/
 
 ReactDOM.render(
     <App/>,
