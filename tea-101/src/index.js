8d94391b2eef90430d5ac738cceebd25ce130688
katawohl <katawohl@gmail.com>
2020-05-31 11:43:12 +0200

    changed class attribute in JSX to className

--- tea-101/src/index.js
+++ tea-101/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import img from './img.jpg';
 
 const Button = (props) => (
-<button class="mainButton" onClick={props.onClickFunction}>Check 'em out!</button>
+<button className="mainButton" onClick={props.onClickFunction}>Check 'em out!</button>
 );
 
 const ImageContent = () => (
-<div className="imgDiv">
-                <img class="roundImg" alt="image of CEO" src={img}></img>
-                <div class="imgText">
+<div classNameName="imgDiv">
+                <img className="roundImg" alt="image of CEO" src={img}></img>
+                <div className="imgText">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>                
             </div>
@@ -19,23 +19,23 @@ const ImageContent = () => (
 class TeaDetails extends React.Component {
     render() {
         return (
-        <div class="secondaryDiv">
+        <div className="secondaryDiv">
             <h2>Tea 101</h2>
-            <div class="contentDiv">
-                <div class="buttonDiv">
-                    <button class="teaButton">Some text here</button>
-                    <button class="teaButton">Some text here</button>
-                    <button class="teaButton">Some text here</button>
-                    <button class="teaButton">Some text here</button>
+            <div className="contentDiv">
+                <div className="buttonDiv">
+                    <button className="teaButton">Some text here</button>
+                    <button className="teaButton">Some text here</button>
+                    <button className="teaButton">Some text here</button>
+                    <button className="teaButton">Some text here</button>
                 </div>
-                <div class="parDiv">
+                <div className="parDiv">
                     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet congue laoreet. Nunc euismod purus ac justo mollis pulvinar. Vivamus volutpat urna nisi, at congue lorem fermentum ac. Morbi sit amet nunc non orci convallis posuere eu vel diam. Vestibulum facilisis condimentum quam, sed consectetur purus pulvinar a. Praesent non eros rhoncus, ultricies quam vitae, vestibulum diam. Praesent nec dolor lectus.</p>
 
                     <p>Nulla et arcu et urna tempus cursus et at nibh. Morbi ornare augue eu urna tempor, non tincidunt nisl ullamcorper. Suspendisse sit amet diam massa. Quisque vitae rhoncus lectus. Nulla efficitur elit viverra accumsan hendrerit. Quisque sit amet nisl sed est malesuada ornare a ac metus. Suspendisse placerat mauris quis purus tempus facilisis. Aenean tristique sem in luctus congue. Nunc et elit erat. Nunc nec turpis condimentum, tincidunt tellus in, cursus ipsum. Vivamus pharetra velit eu tortor fringilla congue. Ut id cursus lorem. Nunc pulvinar, est vel pharetra varius, nunc neque pretium odio, eu lacinia nibh turpis id dui. Proin dui urna, pellentesque vitae purus et, feugiat suscipit justo. Proin at lacinia felis, eu ultrices orci.</p>
                 </div>      
             </div> 
             <ImageContent />
-            <button class="opinionBtn">Leave an opinion</button>
+            <button className="opinionBtn">Leave an opinion</button>
         </div>
         );
     }
@@ -49,7 +49,7 @@ class App extends React.Component {
     render() {
         const showTeasAction = () => this.setState({showTeas: true});
         return (
-            <div class="mainDiv" >
+            <div className="mainDiv" >
                 <h1>Delicious Delights</h1>
                 <Button onClickFunction={showTeasAction} />
                 { this.state.showTeas && (<TeaDetails/>) }
