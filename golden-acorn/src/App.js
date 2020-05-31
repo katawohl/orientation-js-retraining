71620d0eb12296c20220fdce29ae3ccbf097a4b2
katawohl <katawohl@gmail.com>
2020-05-31 09:07:08 +0200

    created basic view of the app

--- golden-acorn/src/App.js
+++ golden-acorn/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Button from './Button.js'
+import Display from './Display.js'
 
 const App = ({title}) => (
     <div>
         <h1>{title}</h1>
+        <Button />
+        <Display />
         <Button />
     </div>
 );
---
+++ golden-acorn/src/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = () => (
+    <div>
+        This the display.
+    </div>
+);
+
+export default Display
