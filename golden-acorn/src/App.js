c1d2422d871f34ababa6199b6c5ee41abe2164cf
katawohl <katawohl@gmail.com>
2020-05-31 09:13:34 +0200

    added counter state to app and passed it to Display component

--- golden-acorn/src/App.js
+++ golden-acorn/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button.js'
 import Display from './Display.js'
 
-const App = ({title}) => (
+const App = ({title}) => {
+    const [counter, setCounter] = useState(0);
+
+    return (
     <div>
         <h1>{title}</h1>
         <Button />
-        <Display />
+        <Display message={counter} />
         <Button />
     </div>
-);
+    );
+};
 
 export default App;
--- golden-acorn/src/Display.js
+++ golden-acorn/src/Display.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Display = () => (
+const Display = (props) => (
     <div>
-        This the display.
+       {props.message}
     </div>
 );
 
