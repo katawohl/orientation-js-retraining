4e027eb31d87ca514d4be51b79e7993a361bb930
katawohl <katawohl@gmail.com>
2020-05-31 09:50:24 +0200

    renamed file containting App component to SimpleGoldenAcornApp

--- golden-acorn/src/index.js
+++ golden-acorn/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './SimpleGoldenAcornApp.js';
 
 ReactDOM.render(
     <App title="The Golden Acorn" />,
