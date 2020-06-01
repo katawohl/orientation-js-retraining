cc2ed197e558dae389296756f89670b620a08496
katawohl <katawohl@gmail.com>
2020-06-01 10:39:27 +0200

    created OpinionButton component

---
+++ tea-101/src/OpinionButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OpinionButton = () => {
+    return (
+        <div className='opBtnDiv'>
+            <button className="opinionBtn">Leave an opinion</button>
+        </div>
+    );
+}
+
+
+export default OpinionButton
